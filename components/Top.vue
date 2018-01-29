213fe227353b512711ae615d9407f3d77b30ad42
Mac Diphot <diphot@iMac.local>
2018-01-29 18:24:41 -0300

    Algunos estilos

--- components/Top.vue
+++ components/Top.vue
@@ -1,19 +1,19 @@
 <template>
  <div>
- 	<div class="alerta-plan">
- 		<p>Está utilizando el plan básico. Acceda a normativas ilimitadas</p>
- 	</div>
-	  <header>
+ 	<nuxt-link to="" class="alerta-plan">
+ 		<p>Está utilizando el plan básico. <strong>Acceda a normativas ilimitadas</strong></p>
+ 	</nuxt-link>
+	  <header class="main__header">
 	    <div class="container">
 	      <button class="hamburger" type="button">
 	        <span class="hamburger-box">
 	          <span class="hamburger-inner"></span>
 	        </span>
 	      </button>
-	      <button><nuxt-link to="/buscar">Buscar</nuxt-link></button>
+	      <button class="buscar"><nuxt-link to="/buscar" class="rounded__btn--full">Buscar</nuxt-link></button>
 	    </div>
 	  </header>
-	  <nav>
+	  <nav class="main__nav">
 	  	<ul>
 	  		<li><nuxt-link to="/inicio">Inicio</nuxt-link></li>
 	  		<li><nuxt-link to="/buscar">Buscar</nuxt-link></li>
