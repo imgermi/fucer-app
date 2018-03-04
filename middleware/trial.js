56778eec2113c66cc98bca297d900b3244647554
Andy <andresdameson@gmail.com>
2018-03-04 18:33:05 -0300

    Mantiene el plan premium hasta un més después de vencido a pago

--- middleware/trial.js
+++ middleware/trial.js
@@ -5,7 +5,17 @@ export default function ({store, app, route}) {
 
     // Restringimos el acceso al contenido a los usuarios con plan
     // básico con trial vencido
-    if (app.$auth.state.user.pago!==1 && route.name!=='trial-vencido') {
+    let usuarioPremium = false
+    if(app.$auth.state.user.pago==1){
+      usuarioPremium = true
+    }else{
+      let premiumHasta = moment(app.$auth.state.user.pago_fecha).add(1, 'month')
+      let hoy = moment()
+      if (premiumHasta.diff(hoy, 'days') >= 0) {
+        usuarioPremium = true
+      }
+    }
+    if (!usuarioPremium && route.name!=='trial-vencido') {
       let fechaActivacion = moment(app.$auth.state.user.activo_fecha)
       let hoy = moment()
       if (hoy.diff(fechaActivacion, 'days') > 7) {
