3f981c8afb5e8762221418b7dbdb7928cadb3a25
Andy <andresdameson@gmail.com>
2018-07-14 20:45:32 -0300

    Arregla botón para volver de la cabecera

--- components/SecondaryTop.vue
+++ components/SecondaryTop.vue
@@ -1,11 +1,11 @@
 <template>
   <header class="secondary-top">
     <div class="container">
-      <nuxt-link :to="{ name: 'index' }" class="cancelar__btn">
+      <a herf="#" class="cancelar__btn" @click.prevent="$router.go(-1)">
         <svg width="18" height="34" viewBox="0 0 18 34" fill="none">
           <path d="M16 0L0 16L16 32" transform="translate(1 1)" stroke="#95989A"/>
         </svg>
-      </nuxt-link>
+      </a>
       <div class="paso">
         <p class="paso__nro">Paso {{ nroPaso }}/3</p>
         <span class="paso__titulo">{{ tituloPaso }}</span>
