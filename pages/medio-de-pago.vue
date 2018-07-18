f1819700b4bd69c8c019b12da73e0e7fd39011db
Mac Diphot <diphot@iMac.local>
2018-07-18 12:55:58 -0300

    Agrego mensajes usuario

--- pages/medio-de-pago.vue
+++ pages/medio-de-pago.vue
@@ -3,9 +3,17 @@
     <SecondaryTop :nroPaso="nroPaso" :tituloPaso="tituloPaso"/>
     <section class="band">
       <div class="container">
+        <div v-if="$auth.state.loggedIn" class="user">
+          <span>Tus datos personales</span>
+          <small>{{ $auth.state.user.nombre }}</small>
+          <small>{{ $auth.state.user.email }}</small>
+        </div>
         <p>Para acceder a sus 15 días gratis, tendrá que suscribirse al plan seleccionado.</p>
         <p>No se preocupe. Cancele antes del {{ moment().add(15, 'days').format('D/M/Y') }} y <strong>no se le cobrará ningún cargo.</strong></p>
         <nuxt-link :to="{ name: 'tarjeta-de-credito' }" class="rounded__btn--full blue">Agregar tarjeta de crédito</nuxt-link>
+        <div v-if="$auth.state.loggedIn">
+          <span class="signup__agregados">¿Quiere iniciar con otra cuenta? <a @click="logout()">Cerrar Sesión</a></span>
+        </div>
       </div>
     </section>
   </div>
@@ -33,6 +41,12 @@ export default {
       meta: [
         { hid: 'description', name: 'description', content: '' }
       ],
+    }
+  },
+  methods: {
+    async logout () {
+      await this.$auth.logout()
+      this.$router.push("/")
     }
   }
 }
