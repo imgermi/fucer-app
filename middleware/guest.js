e1a823d13bfdc06cb6f52bab2506ecbafffd4853
Andy <andresdameson@gmail.com>
2018-07-22 13:33:13 -0300

    Corrije middelware guest

--- middleware/guest.js
+++ middleware/guest.js
@@ -2,7 +2,7 @@ export default function ({store, app, route}) {
 
   // Bloqueo el acceso si el usuario está logueado
   if (app.$auth.state.loggedIn) {
-  	if (store.getters.usuarioPremium) {
+  	if (store.getters.usuarioPremium || store.getters.esTrial) {
     	app.router.push({name: 'inicio'})
   	} else {
     	app.router.push({name: 'medio-de-pago'})
