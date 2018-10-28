45918a2add4173e35b5868ef5fee24067a39eae3
imgermi <juanmartingermano@gmail.com>
2018-10-28 18:44:13 -0300

    Pantalla de estado de suscripcion

---
+++ pages/estado-suscripcion.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="confirme-su-email">
+    <SecondaryTop/>
+    <section class="band">
+      <div class="container">
+        <h1 class="intro__heading">Suscripción en revisión</h1>
+        <h2 class="sub__heading">Mercadopago indica que Visa necesita 48hs hábiles para aprobar su tarjeta de crédito.</h2>
+        <nuxt-link to="" class="rounded__btn--medium blue">
+          Cambiar tarjeta
+        </nuxt-link>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import SecondaryTop from '~/components/SecondaryTop.vue'
+
+export default {
+  layout: 'signup',
+  components: {
+    SecondaryTop
+  },
+  auth: false,
+  data() {
+    return {
+      title: 'Estado de suscripción',
+    }
+  },
+  head () {
+    return {
+      title: this.title,
+      meta: [
+        { hid: 'description', name: 'description', content: '' }
+      ],
+    }
+  }
+}
+</script>
+
+<style lang="sass">@import 'sass/pages/confirme-su-email.sass'</style>
