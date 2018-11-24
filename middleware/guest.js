c6a8862a70881f3d7bd9f418aef168353ef25999
Andy <andresdameson@gmail.com>
2018-11-24 18:46:18 -0300

    Avisa a los usuarios cuando hay problemas o demoras con el pago

--- middleware/guest.js
+++ middleware/guest.js
@@ -5,7 +5,20 @@ export default function ({store, app, route}) {
   	if (store.getters.usuarioPremium || store.getters.esTrial) {
     	app.router.push({name: 'inicio'})
   	} else {
-    	app.router.push({name: 'medio-de-pago'})
+  		// Hay usuarios que no figuran como premium en el store pero que desde
+  		// la API sí lo son porque el cobro del último invoice no se hizo en el
+  		// día (ya sea por falta de una autorización, por un proceso de gestión
+  		// desde VISA por ejemplo o por falta de fondos).
+  		//
+  		// Estos usuarios van a figurar como premium desde la API porque se tiene
+  		// encuenta el estad del invoice (en este caso pending o dunning) pero se
+  		// les bloquea el acceso igualmente hasta que se confirme el pago de la
+  		// suscripción.
+		if (store.state.auth.user.es_premium) {
+	  		app.router.push({name: 'estado-suscripcion'})
+		}else{
+    		app.router.push({name: 'medio-de-pago'})
+		}
   	}
   }
 }
--- pages/estado-suscripcion.vue
+++ pages/estado-suscripcion.vue
@@ -4,8 +4,8 @@
     <section class="band">
       <div class="container">
         <h1 class="intro__heading">Suscripción en revisión</h1>
-        <h2 class="sub__heading">Mercadopago indica que Visa necesita 48hs hábiles para aprobar su tarjeta de crédito.</h2>
-        <nuxt-link to="" class="rounded__btn--medium blue">
+        <h2 class="sub__heading">{{ mensaje }}</h2>
+        <nuxt-link :to="{name: 'modificar-tarjeta'}" class="rounded__btn--medium blue">
           Cambiar tarjeta
         </nuxt-link>
       </div>
@@ -25,6 +25,18 @@ export default {
   data() {
     return {
       title: 'Estado de suscripción',
+      mensajes: {
+        pending: 'Un pago esta agendado para ser cobrado próximamente.',
+        dunning: 'Los intentos de cobro anteriores fueron rechazados, MercadoPago está reintentando el cobro para obtener una aprobación.',
+        paid: 'El pago de la suscripción fue realizado exitosamente.',
+        unpaid: 'Luego de todos los intentos de cobro, no fue posible obtener una aprobación. Ya no se realizarán más reintentos. Le aconsejamos que actualize su tarjeta y que vuelva a suscribirse.',
+        cancelled: 'Se canceló el pago programado antes de ser cobrado.'
+      }
+    }
+  },
+  computed: {
+    mensaje () {
+      return this.mensajes[this.$store.state.auth.user.suscripcion.estado_invoice]
     }
   },
   head () {
