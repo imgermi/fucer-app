5e5e5896cf9ba8b7b66314a4e5b6f8e982f8887e
Andy <andresdameson@gmail.com>
2019-10-23 15:03:08 -0300

    Corrije loop de redireccionamiento al iniciar sesión

--- middleware/guest.js
+++ middleware/guest.js
@@ -1,17 +1,22 @@
 export default function ({store, app, route}) {
 
   // Bloqueo el acceso si el usuario está logueado
+  let redirigir = route.name
   if (app.$auth.loggedIn) {
-  	if (store.getters.usuarioPremium || store.getters.esTrial) {
-    	app.router.push({name: 'inicio'})
-  	} else {
+    if (store.getters.usuarioPremium || store.getters.esTrial) {
+      redirigir = 'inicio'
+    } else {
       // Si tiene suscripción es que no pagó, la suspendió, o está teniedo un problema con el pago
-  		if (store.state.auth.user.suscripcion.id) {
-  	  		app.router.push({name: 'estado-suscripcion'})
+      if (store.state.auth.user.suscripcion.id) {
+        redirigir = 'estado-suscripcion'
       // Usuario sin registrarse
-  		}else{
-      		app.router.push({name: 'medio-de-pago'})
-  		}
-  	}
+      }else{
+        redirigir = 'medio-de-pago'
+      }
+    }
+  }
+  
+  if (route.name !== redirigir) {
+    app.router.push({name: redirigir})
   }
 }
