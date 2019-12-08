261d41d6987c261025066f7d95a57d2b242d999e
Andy <andresdameson@gmail.com>
2019-12-08 20:51:28 -0300

    a11y: agrega herramienta para auditar problemas de accesibilidad

---
+++ plugins/axe.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+
+if (process.env.NODE_ENV !== 'production') {
+  const VueAxe = require('vue-axe')
+  Vue.use(VueAxe, {
+    clearConsoleOnUpdate: true,
+    // https://github.com/dequelabs/axe-core/blob/master/doc/API.md#api-name-axeconfigure
+    config: {
+      reporter: "no-passes"
+    }
+  })
+}
