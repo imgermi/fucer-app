f5626f6bcf5f5ccde8809dfb66a02629b6d4bf2b
Andy <andresdameson@gmail.com>
2019-12-08 23:12:24 -0300

    a11y: agrega extensión de eslint para auditar accesibilidad

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,11 +11,12 @@ module.exports = {
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     'plugin:vue/essential',
+    'plugin:vue-a11y/recommended',
   ],
-  // required to lint *.vue files
   plugins: [
-    'vue'
+    // required to lint *.vue files
+    'vue',
+    'vue-a11y',
   ],
-  // add your custom rules here
   rules: {}
 }
