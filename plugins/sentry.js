67e0b8c3157238bd77f00e1d174a5917dd4c549d
Andy <andresdameson@gmail.com>
2020-05-17 19:30:08 -0300

    [a11y] Abre el menú y desplaza el foco con el skip link

--- plugins/sentry.js
+++ plugins/sentry.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import * as Sentry from '@sentry/browser';
 import { Vue as VueIntegration } from '@sentry/integrations';
 
-Sentry.init({
-  dsn: 'https://763e54ccc380411ab56392337ce97f97@o392771.ingest.sentry.io/5240921',
-  integrations: [new VueIntegration({ Vue, attachProps: true })],
-});
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: 'https://763e54ccc380411ab56392337ce97f97@o392771.ingest.sentry.io/5240921',
+    integrations: [new VueIntegration({ Vue, attachProps: true })],
+  });
+}
+
