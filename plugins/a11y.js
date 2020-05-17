4309cd2d4db90024b50a13447e5544d0c32c4ab5
Andy <andresdameson@gmail.com>
2020-05-17 11:41:24 -0300

    [ally] Destaca foco cuando se navega con teclado

--- plugins/a11y.js
+++ plugins/a11y.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
+
+// Really tell what’s going on in your application to people using screen readers.
 import VueAnnouncer from 'vue-announcer'
+
+// Polyfill for `:focus-visible`
+import 'focus-visible'
 
 export default function({ app: {router} }) {
 	Vue.use(VueAnnouncer, {
