95c582782565e0fb80b5a9321cbe03aed6cb3939
Andy <andresdameson@gmail.com>
2020-05-26 17:28:05 -0300

    [chore] Actualiza dependencias sin breaking changes

--- plugins/axe.js
+++ plugins/axe.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 
 if (process.env.NODE_ENV !== 'production') {
-  const VueAxe = require('vue-axe')
+  const VueAxe = require('vue-axe').default
   Vue.use(VueAxe, {
     clearConsoleOnUpdate: true,
     // https://github.com/dequelabs/axe-core/blob/master/doc/API.md#api-name-axeconfigure
