7765d1f1fd6219e06068777d9b991ac5dc68af67
Andy <andresdameson@gmail.com>
2020-05-26 21:03:42 -0300

    [rendimiento] Achica el bundle quitando locales de vee-validate

--- plugins/vue-validate.js
+++ plugins/vue-validate.js
@@ -1,12 +1,17 @@
+// http://vee-validate.logaretm.com/v2/concepts/bundle-size.html#minimal-bundle
 import Vue from 'vue';
-import VeeValidate from 'vee-validate';
-import es from 'vee-validate/dist/locale/es.js';
 
+import { Validator, install as VeeValidate } from 'vee-validate/dist/vee-validate.minimal.esm.js';
+import * as veeRules from 'vee-validate/dist/rules.esm.js';
+import veeEs from 'vee-validate/dist/locale/es';
 
-Vue.use(VeeValidate, {
-	delay: 500,
-	locale: 'es',
-	dictionary: {
-		es: es
-	}
+// Add rules
+Object.keys(veeRules).forEach(rule => {
+	Validator.extend(rule, veeRules[rule]);
 });
+
+// Merge the messages.
+Validator.localize('es', veeEs);
+
+// install the plugin
+Vue.use(VeeValidate);
