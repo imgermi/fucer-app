2111bc7cbbc5f3cbc18b4a8c66743a72d9142cb3
Andy <andresdameson@gmail.com>
2020-05-26 23:55:26 -0300

    [rendimiento] Achica aún más el bundle quitando reglas de vee-validate

--- plugins/vue-validate.js
+++ plugins/vue-validate.js
@@ -2,13 +2,14 @@
 import Vue from 'vue';
 
 import { Validator, install as VeeValidate } from 'vee-validate/dist/vee-validate.minimal.esm.js';
-import * as veeRules from 'vee-validate/dist/rules.esm.js';
+import { required, email, confirmed } from 'vee-validate/dist/rules.esm.js';
 import veeEs from 'vee-validate/dist/locale/es';
 
 // Add rules
-Object.keys(veeRules).forEach(rule => {
-	Validator.extend(rule, veeRules[rule]);
-});
+// http://vee-validate.logaretm.com/v2/guide/rules.html
+Validator.extend('required', required);
+Validator.extend('email', email);
+Validator.extend('confirmed', confirmed);
 
 // Merge the messages.
 Validator.localize('es', veeEs);
