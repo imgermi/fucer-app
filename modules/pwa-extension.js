8dd6a68553dd6343334ef54cc449b9bae67e106c
Andy Damesón <andresdameson@gmail.com>
2020-06-19 11:26:01 -0300

    [fix] Corrije precaché de PWA que precacheaba build anterior

--- modules/pwa-extension.js
+++ modules/pwa-extension.js
@@ -1,17 +1,26 @@
-const { getManifest } = require('workbox-build')
+const path = require('path')
+const { injectManifest } = require('workbox-build')
 
 export default function nuxtWorkboxExtension() {
-  this.nuxt.hook('build:before', async (nuxt, buildOptions) => {
-    const { manifestEntries } = await getManifest({
-      globDirectory: this.options.srcDir + '/.nuxt/dist/client',
+  this.nuxt.hook('build:before', async (nuxt) => {
+    let cachingExtensions = nuxt.options.pwa.workbox.cachingExtensions || []
+    cachingExtensions = Array.isArray(cachingExtensions) ? cachingExtensions : [cachingExtensions]
+    nuxt.options.pwa.workbox.cachingExtensions = cachingExtensions.concat(['~/modules/pwa-extension-inject.js'])
+  })
+  this.nuxt.hook('build:compiled', async () => {
+    const swFile = path.resolve(this.options.srcDir, this.options.dir.static || 'static', 'sw.js')
+    await injectManifest({
+      globDirectory: this.options.srcDir + '/.nuxt/dist/',
       globPatterns: [
         '**/*.{js,css}'
       ],
+      swSrc: swFile,
+      swDest: swFile,
       modifyURLPrefix: {
-        '': '/_nuxt/'
+        'client/': '/_nuxt/',
+        'server/': '/_nuxt/'
       },
+      injectionPoint: 'self.__PWA_EXTENSION'
     })
-    const preCaching = nuxt.options.pwa.workbox.preCaching || []
-    nuxt.options.pwa.workbox.preCaching = preCaching.concat(manifestEntries)
   })
 }
