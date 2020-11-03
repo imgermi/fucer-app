f417852a9d7d007cece1794d5c7c032ee36fd190
Juan Germano <j@thewhoknows.com>
2020-11-03 15:44:45 -0300

    nuevo link suplemento

--- pages/suplemento.vue
+++ pages/suplemento.vue
@@ -9,6 +9,15 @@
         <div class="normativas-container">
           <article class="normativa-module">
             <div class="main">
+              <h4 ref="pageFocusTarget"><a href="https://mailchi.mp/ce04acf5b705/suplemento-registral-fucernet-1427873" target="_blank">Suplemento Registral Nro. 6</a></h4>
+              <!-- <p><a href="">bajada</a></p> -->
+            </div>
+            <div class="extra">
+              <small><a>Noviembre 2020</a></small>
+            </div>
+          </article>
+          <article class="normativa-module">
+            <div class="main">
               <h4 ref="pageFocusTarget"><a href="https://mailchi.mp/7b2d16ad0e57/suplemento-registral-fucernet-1423553" target="_blank">Suplemento Registral Nro. 5</a></h4>
               <!-- <p><a href="">bajada</a></p> -->
             </div>
