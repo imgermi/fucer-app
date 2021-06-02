dfae601e7a09d11990d5eb68f0b784026729f5bc
Juan Germano <j@thewhoknows.com>
2021-06-02 12:38:14 -0300

    cambio texto

--- pages/inicio.vue
+++ pages/inicio.vue
@@ -99,7 +99,7 @@
             <article class="normativa-module">
               <div class="main">
                 <h4><a href="http://servicios.infoleg.gob.ar/infolegInternet/anexos/70000-74999/74456/texact.htm" target="_blank">Resolución MJ y DH Nº 314/2002</a></h4>
-                <p><a href="">Resolución de aranceles vigente a partir del 01/06/2019, incluye Resol MJ y DH Nº 323/2019</a></p>
+                <p><a href="">Resolución de aranceles vigente a partir del 01/06/2021, incluye Resol MJ y DH Nº 437/2021</a></p>
               </div>
               <div class="extra">
                 <small><a>Enlace externo</a></small>
